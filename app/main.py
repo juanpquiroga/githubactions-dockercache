ecd17ad3e87743ac930280dc680d98849a916942
Juan Pablo Quiroga <juan.quiroga@liftit.co>
2022-07-11 07:48:28 -0500

    Probar gpr 3

--- app/main.py
+++ app/main.py
@@ -13,5 +13,5 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    print("read_item")
+    print("read_item 1")
     return {"item_id": item_id, "q": q}
